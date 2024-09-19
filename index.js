ff7853ec94b7499d6390c867a3f9f2a9b7a5a099
Ethan Obott <ethanobott6002@gmail.com>
2024-09-19 19:44:25 +0100

    display all items in vending machine in rows

--- index.js
+++ index.js
@@ -7,6 +7,50 @@ class Item {
     }
 }
 
+//!CREATING AND ADDING HEADER
+const h1 = document.createElement("h1");
+document.body.appendChild(h1);
+h1.textContent = "Vending Machine";
+
+
+
+//@! TABLE HEADER
+
+//function that will add a border to any element that is passed to it 
+const addBorderToElem = (elem) => {
+    elem.style.border = "1px solid black";
+}
+
+
+//creating and appending table to body
+const table = document.createElement("table");
+document.body.appendChild(table);
+addBorderToElem(table);
+
+
+//creating and appending table head to table;
+const thead = document.createElement("thead");
+table.appendChild(thead);
+
+//creating and appending table row to table
+const trHead = document.createElement("tr");
+thead.appendChild(trHead);
+
+//creating table headers
+const tableHeaders = ["Item Code", "Item Name", "Item Price", "Quantity In Stock"];
+
+for (let index = 0; index < tableHeaders.length; index++) {
+    const td = document.createElement("td"); //creating a cell
+    trHead.appendChild(td); //adding a cell to the row
+    addBorderToElem(td); //adding a border to the cell
+    td.textContent = tableHeaders[index]; //adding the correct header name to the cell
+};
+
+//@!BODY
+const tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+//items list that contains objects from "Items" class
 const items = [
     new Item("A1", "Chips", 3.00, 5),
     new Item("A2", "Pretzels", 5.00, 8),
@@ -22,42 +66,19 @@ const items = [
     new Item("D3", "Trail Mix", 5.50, 13)
 ];
 
-//!CREATING AND ADDING HEADER
-const h1 = document.createElement("h1");
-document.body.appendChild(h1);
-h1.textContent = "Vending Machine";
+//creating rows for each item
+items.forEach((item) => {
+    const tr = document.createElement("tr"); //create a row
+    tbody.appendChild(tr); //add it to the body
 
-const borderStyle = "1px solid black";
-
-// @!CREATING AND ADDING TABLES TO THE PAGE
-//creating and appending table to body
-const table = document.createElement("table");
-document.body.appendChild(table);
-table.style.border = borderStyle;
-
-//creating and appending table head to table;
-const thead = document.createElement("thead");
-table.appendChild(thead);
-
-//creating and appending table row to table
-const tr = document.createElement("tr");
-thead.appendChild(tr);
-
-//creating and appending 4 tables cells to the row
-
-const tableHeaders = ["Item Code", "Item Name", "Item Price", "Quantity In Stock"];
-
-for (let index = 0; index < tableHeaders.length; index++) {
-    const td = document.createElement("td"); //creating a cell
-    tr.appendChild(td); //adding a cell to the row
-    td.style.border = borderStyle; //adding a border to the cell
-    td.textContent = tableHeaders[index]; //adding the correct header name to the cell
-};
-
-
-
-
-
+    //creating 4 cells 
+    for (let x of Object.values(item)) {
+        const td = document.createElement("td"); //create cell
+        tr.appendChild(td); //add cell to row
+        td.textContent = x; //populate cell
+        addBorderToElem(td);//add border to cell
+    }
+});
 
 
 
